f2b57b9ccfe85f52b1f3a67a1c4aa9ef4f4d6dbc
dgolov <dmitriy.evgenyevich@gmail.com>
2024-08-20 00:01:52 +0300

    Added logs

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ from connections.grpc_server import serve
 from fastapi import FastAPI
 from routes import routes
 
+import uvicorn
+import threading
+
+
 app = FastAPI()
 app.include_router(routes)
 
 
 @app.on_event("startup")
 def startup_event():
-    import threading
     threading.Thread(target=serve).start()
 
 
 if __name__ == "__main__":
-    import uvicorn
+    print("Start FastAPI app for testing gRPC")
     uvicorn.run(app, host="127.0.0.1", port=8000)
 
